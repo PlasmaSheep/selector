f8518f21a4b4f80e40ef37e56e47fdb54282bc8d
PlasmaSheep <plasmasheep@gmail.com>
2013-07-03 11:03:25 -0700

    First commit

---
+++ selecta.py
@@ -0,0 +1,4 @@
+#Things this should do:
+#1. Read a config file to see what shows to download and when the last show downloaded was
+#2. Go to rinse website for each of the shows
+#3. Download all undownloaded episodes
